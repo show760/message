fc1bcc10fb2fd32b00c1afc2c78670446d699659
James-Yuan <show760@hotmail.com>
2014-08-06 08:52:03 +0800

    update README.md & testmessage.sql

--- README.md
+++ README.md
@@ -1,18 +1,41 @@
-Mysql new user sql
+ Mysql new user SQL指令
 -----------------------
-###james
+### james
+```
 GRANT USAGE ON *.* TO 'james'@'localhost' IDENTIFIED BY PASSWORD '*A4B6157319038724E3560894F7F932C8886EBFCF';
-
 GRANT ALL PRIVILEGES ON `james`.* TO 'james'@'localhost' WITH GRANT OPTION;
-
-###testmessage
+```
+### testmessage
+```
 GRANT USAGE ON *.* TO 'testmessage'@'localhost' IDENTIFIED BY PASSWORD '*A4B6157319038724E3560894F7F932C8886EBFCF';
-
 GRANT ALL PRIVILEGES ON `testmessage`.* TO 'testmessage'@'localhost';
-
-New DATABASE testmessage IF NOT EXISTS
+```
+New DATABASE **testmessage** and **james** IF NOT EXISTS
 ----------------------
+```
 CREATE DATABASE IF NOT EXISTS `testmessage` COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `james` COLLATE utf8_general_ci;
+```
 
-用testmessage.sql將資料匯入testmessage資料庫
-用james.sql將資料匯入james資料庫
+### 匯入資料表
+使用testmessage.sql將資料匯入testmessage資料庫
+
+使用james.sql將資料匯入james資料庫
+
+### nginx server definition for example:
+
+```
+server {
+    server_name message;
+    listen 80;
+    root /home/user/www/message;
+    location / {
+        try_files $uri @php;
+    }
+    location @php {
+        fastcgi_split_path_info ^(.+)(\?/.+)$;
+        fastcgi_pass unix:/var/run/php5-fpm.sock;
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME $document_root/index.php;
+    }
+}
--- testmessage.sql
+++ testmessage.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主機: localhost
--- 產生時間： 2014 年 08 月 05 日 17:00
+-- 產生時間： 2014 年 08 月 06 日 08:51
 -- 伺服器版本: 5.5.37-1
 -- PHP 版本： 5.6.0RC2
 
@@ -19,12 +19,11 @@ SET time_zone = "+00:00";
 --
 -- 資料庫： `testmessage`
 --
+
 -- --------------------------------------------------------
 
 --
 -- 資料表結構 `message`
---
--- 建立時間: 2014 年 08 月 05 日 08:53
 --
 
 CREATE TABLE IF NOT EXISTS `message` (
@@ -32,30 +31,20 @@ CREATE TABLE IF NOT EXISTS `message` (
   `message_Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `message_Name` varchar(10) NOT NULL,
   `message_Text` tinytext NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=154 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=149 ;
 
 --
 -- 資料表的匯出資料 `message`
 --
 
 INSERT INTO `message` (`message_Id`, `message_Time`, `message_Name`, `message_Text`) VALUES
-(1, '2014-07-16 03:12:27', 'panda', 'test'),
 (2, '2014-07-16 03:41:12', 'james', 'hello'),
-(3, '2014-07-16 04:29:47', '23132', '4654654'),
-(125, '2014-07-16 04:29:19', 'James', 'TEST!!!'),
-(126, '2014-07-16 04:30:09', 'james', 'TEST'),
-(127, '2014-07-16 05:06:32', 'panda', '測試修改內容                     '),
-(140, '2014-07-28 03:07:01', 'asdgsdag', '731152432'),
-(143, '2014-07-29 08:38:56', 'asdfsadf', '165620'),
-(144, '2014-07-31 07:09:34', '0731', '150929'),
 (145, '2014-08-01 02:06:21', 'phpunit', 'testupd');
 
 -- --------------------------------------------------------
 
 --
 -- 資料表結構 `remessage`
---
--- 建立時間: 2014 年 08 月 05 日 08:53
 --
 
 CREATE TABLE IF NOT EXISTS `remessage` (
@@ -64,19 +53,13 @@ CREATE TABLE IF NOT EXISTS `remessage` (
   `remessage_Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `remessage_Name` varchar(10) NOT NULL,
   `remessage_Text` tinytext NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
 
 --
 -- 資料表的匯出資料 `remessage`
 --
 
 INSERT INTO `remessage` (`remessage_Id`, `message_Id`, `remessage_Time`, `remessage_Name`, `remessage_Text`) VALUES
-(2, 2, '2014-07-25 08:29:29', 'James', '回覆                     '),
-(6, 140, '2014-07-28 03:24:31', 'test', '0728'),
-(7, 127, '2014-07-28 05:48:45', 'James', '123321'),
-(8, 1, '2014-07-28 06:27:26', 'panda', 'panda                     '),
-(9, 1, '2014-07-30 02:50:57', 'kobe', 'text'),
-(10, 1, '2014-07-30 05:44:43', 'boy', '1024'),
 (11, 2, '2014-07-31 08:09:02', 'kobe', 'testphpunit');
 
 --
@@ -103,12 +86,12 @@ ALTER TABLE `remessage`
 -- 使用資料表 AUTO_INCREMENT `message`
 --
 ALTER TABLE `message`
-MODIFY `message_Id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=154;
+MODIFY `message_Id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=149;
 --
 -- 使用資料表 AUTO_INCREMENT `remessage`
 --
 ALTER TABLE `remessage`
-MODIFY `remessage_Id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
+MODIFY `remessage_Id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
 --
 -- 已匯出資料表的限制(Constraint)
 --
